e996c951c9f4a90b7c7b75c11e612d44a3955be6
HP <smahaarunachalam@gmail.com>
2023-03-30 16:12:09 +0530

    Add project files.

---
+++ Problem2.cs
@@ -0,0 +1,209 @@
+﻿/*Polymorphism
+
+A friend of yours wants to create a special calculator.
+He only wants two operations to be handled by that calculator
+i.e. : addition and subtraction.
+The calculator can only accept either 2 numbers or 3 numbers.
+
+There are two functionalities for this calculator -
+1. Normal -> in this role - the calculator works normally
+ - where upon adding the numbers are added,
+while upon subtracting the numbers are subtracted.
+
+2. Opposite -> in this role - the calculator works opposite
+to the normal role - where upon adding the numbers are subtracted,
+ while upon subtracting the numbers are added.
+That friend have approached you for the help, in creating
+ such a calculator.*/
+
+
+namespace TaskMar30
+{
+    class Calculator
+    {
+        //Constructor
+        public Calculator(int mode)
+        {
+            //Check if mode is 1 or 2
+            if (mode == 1)
+            {
+                //Create object of Normal class
+                Normal normal = new Normal();
+                Console.WriteLine("Normal Mode");
+                Console.WriteLine("Enter operation \n1.Add \n2.Subtract");
+                //Read user input and convert to int
+                int operation = Convert.ToInt32(Console.ReadLine());
+                //Check if operation is 1 or 2
+                if (operation == 1)
+                {
+                    Console.WriteLine("Enter 2 or 3 numbers");
+                    //Getting user Input in a row by giving in space and convert to list
+                    List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+                    //Check if list has 2 or 3 elements
+                    if (numbers.Count == 2)
+                    {
+                        //Call Add method
+                        normal.Add(numbers[0], numbers[1]);
+                    }
+                    else if (numbers.Count == 3)
+                    {
+                        //Call Add method with 3 parameters
+                        normal.Add(numbers[0], numbers[1], numbers[2]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid number of arguments");
+                    }
+
+                }
+                else if (operation == 2)
+                {
+                    Console.WriteLine("Enter 2 or 3 numbers");
+                    //Getting user Input in a row by giving in space and convert to list
+                    List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+                    //Check if list has 2 or 3 elements
+                    if (numbers.Count == 2)
+                    {
+                        //Call Add method
+                        normal.Subtract(numbers[0], numbers[1]);
+                    }
+                    else if (numbers.Count == 3)
+                    {
+                        //Call Add method with 3 parameters
+                        normal.Subtract(numbers[0], numbers[1], numbers[2]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid number of arguments");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid operation");
+                }
+            }
+            else if (mode == 2)
+            {
+                //Create object of Opposite class
+                Opposite opposite = new Opposite();
+                Console.WriteLine("Normal Mode");
+                Console.WriteLine("Enter operation \n1.Add \n2.Subtract");
+                //Read user input and convert to int
+                int operation = Convert.ToInt32(Console.ReadLine());
+                //Check if operation is 1 or 2
+                if (operation == 1)
+                {
+                    Console.WriteLine("Enter 2 or 3 numbers");
+                    //Getting user Input in a row by giving in space and convert to list
+                    List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+                    //Check if list has 2 or 3 elements
+                    if (numbers.Count == 2)
+                    {
+                        //Call Add method
+                        opposite.Add(numbers[0], numbers[1]);
+                    }
+                    else if (numbers.Count == 3)
+                    {
+                        //Call Add method with 3 parameters
+                        opposite.Add(numbers[0], numbers[1], numbers[2]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid number of arguments");
+                    }
+
+                }
+                else if (operation == 2)
+                {
+                    Console.WriteLine("Enter 2 or 3 numbers");
+                    //Getting user Input in a row by giving in space and convert to list
+                    List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+                    //Check if list has 2 or 3 elements
+                    if (numbers.Count == 2)
+                    {
+                        //Call Add method
+                        opposite.Subtract(numbers[0], numbers[1]);
+                    }
+                    else if (numbers.Count == 3)
+                    {
+                        //Call Add method with 3 parameters
+                        opposite.Subtract(numbers[0], numbers[1], numbers[2]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid number of arguments");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid operation");
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid mode");
+            }
+        }
+    }
+    class Normal
+    {
+        //Add method
+        public void Add(int a, int b)
+        {
+            Console.WriteLine("Addition of {0} and {1} is {2}", a, b, a + b);
+        }
+        //Subtract method
+        public void Subtract(int a, int b)
+        {
+            Console.WriteLine("Subtraction of {0} and {1} is {2}", a, b, a - b);
+        }
+        //Add method with 3 parameters
+        public void Add(int a, int b, int c)
+        {
+            Console.WriteLine("Addition of {0}, {1} and {2} is {3}", a, b, c, a + b + c);
+        }
+        //Subtract method with 3 parameters
+        public void Subtract(int a, int b, int c)
+        {
+            Console.WriteLine("Subtraction of {0}, {1} and {2} is {3}", a, b, c, a - b - c);
+        }
+    }
+    class Opposite
+    {
+        //Add method
+        public void Add(int a, int b)
+        {
+            Console.WriteLine("Addition of {0} and {1} is {2}", a, b, a - b);
+        }
+        //Subtract method
+        public void Subtract(int a, int b)
+        {
+            Console.WriteLine("Subtraction of {0} and {1} is {2}", a, b, a + b);
+        }
+        //Add method with 3 parameters
+        public void Add(int a, int b, int c)
+        {
+            Console.WriteLine("Addition of {0}, {1} and {2} is {3}", a, b, c, a - b - c);
+        }
+        //Subtract method with 3 parameters
+        public void Subtract(int a, int b, int c)
+        {
+            Console.WriteLine("Subtraction of {0}, {1} and {2} is {3}", a, b, c, a + b + c);
+        }
+    }
+
+    class Problem1
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Enter calculator mode 1.Normal\n2.Opposite");
+            //Read user input and convert to int
+            int mode = Convert.ToInt32(Console.ReadLine());
+            //Create object of Calculator class
+            Calculator calc = new Calculator(mode);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,127 @@
+﻿/*Abstraction:
+
+A company XYZ wants to develop an app which can predict the
+lucky number for a person.
+The name of the app is LuckyNumberPredictor.
+This app consists of a method - upon calling the person gets to know his/her lucky number.
+ This method calls another class —> NumberPredictor - which have the formula for
+ predicting the lucky number of that person. This approach is done so that no-one else gets
+to know the secret formula for this prediction.
+
+LuckyNumberPredictor accepts only one argument i.e: date of birth of the person. The formula
+for predicting the lucky number is quite simple -> a person’s lucky number is calculated by rounding off the date of birth to the nearest Fibonacci number.
+Another thought that XYZ company is  having - is to predict unlucky number as well - but for this — for now, they don’t have any formula ready. 
+But they want to have this idea to be stored in - in their NumberPredictor class. The company is asking you to develop this system.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskToday
+{
+
+    class NumberPredictor
+    {
+        static void fibonacci(string DateofBirth)
+        {
+            //Convert string Dob to int
+            int Dob=int.Parse(DateofBirth);
+            //Method to round off date of birth to nearest fibonacci number
+            //Find nearest fibonacci number of this dob
+            // Base Case
+            if (Dob == 0)
+            {
+                Console.Write(0);
+                return;
+            }
+
+            int first = 0, second = 1;
+            int third = first + second;
+
+            while (third <= Dob)
+            {
+
+                // Update the first
+                first = second;
+
+                // Update the second
+                second = third;
+
+                // Update the third
+                third = first + second;
+            }
+
+            // Store the Fibonacci number
+            // having smaller difference with N
+            int ans = (Math.Abs(third - Dob) >=
+                      Math.Abs(second - Dob)) ?
+                               second : third;
+            Console.Write("Lucky number is"+ans);
+
+        }
+        public static void PredictLuckyNumber(string DateofBirth)
+        {
+            if (DateofBirth == null)
+            {
+                Console.WriteLine("Invalid Date");
+                
+            }
+            else
+            {
+                //Round off Dateof birth to nearest fibonacci number
+                fibonacci(DateofBirth);
+            }
+        }
+        public static void PredictUnLuckyNumber(String DateofBirth)
+        {
+            Console.WriteLine("Will be Implemented in future");
+        }
+    }
+
+    class LuckyNumberPredictor
+    {
+        public static void PredictLuckyNumber(string DateOfBirth)
+        {
+            //Create object of NumberPredictor
+            NumberPredictor.PredictLuckyNumber(DateOfBirth);
+        }
+        public static void PredictUnluckyNumber(String DateofBirth)
+        {
+            NumberPredictor.PredictUnLuckyNumber(DateofBirth);
+        }
+
+    }
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Getting user input
+            Console.WriteLine("Enter Date of Birth");
+            string DateofBirth=Console.ReadLine();
+            //Getting User Choice
+            Console.WriteLine("Enter the option\n1.Predict Lucky Number\n2.PredictUnLucky Number");
+            int choice= Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                //Call PredictLuckyNumber method
+                LuckyNumberPredictor.PredictLuckyNumber(DateofBirth);
+            }
+            else if(choice == 2) {
+                //Call PredictUnluckyNumber method
+                LuckyNumberPredictor.PredictUnluckyNumber(DateofBirth);
+            }
+            else
+            {
+                Console.WriteLine("Wrong Choice");
+            }
+
+        }
+    }
+ 
+}
